0329d54858e3e595df271e2786e87ff940e76d7d
cbobeda <cbobeda@gaming.tech>
2024-11-25 17:00:38 +0100

    Amelioration du systeme de dessin

--- Programation_avance/Programation_avance.cpp
+++ Programation_avance/Programation_avance.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "button.h"
 
 using namespace sf;
 
-RenderWindow window(VideoMode(800,600),"SMFL");
+RenderWindow window(VideoMode(1920,1080),"SMFL");
+Color colorTab [8] = {Color::Black,Color::Blue,Color::Cyan,Color::Green,Color::Magenta,Color::Red,Color::White,Color::Yellow};
+int currentColor = 0;
 int Drawsize = 10;
+button changeColor0(0,0,50,50);
+button changeColor1(100,0,50,50);
+button changeColor2(200,0,50,50);
+button changeColor3(300,0,50,50);
+button changeColor4(400,0,50,50);
+button changeColor5(500,0,50,50);
+button changeColor6(600,0,50,50);
+button changeColor7(700,0,50,50);
 Texture createimage(int x, int y)
 {
     Image image;
@@ -14,7 +25,7 @@ Texture createimage(int x, int y)
     return texture;
 }
 
-Image dessine(int x, int y,int size,Texture texture)
+Image dessine(int x, int y,int size,Color couleur,Texture texture)
 {
     Image imageBuffer = texture.copyToImage();
     if (Mouse::getPosition(window).x > 0 && Mouse::getPosition(window).y > 0)
@@ -25,7 +36,7 @@ Image dessine(int x, int y,int size,Texture texture)
             {
                 if (y > 0 && x > 0)
                 {
-                    imageBuffer.setPixel(x,y,Color::Black);
+                    imageBuffer.setPixel(x,y,couleur);
                 }
             }
         }
@@ -33,9 +44,54 @@ Image dessine(int x, int y,int size,Texture texture)
     return imageBuffer;
     
 }
-
+Texture changeColor(Texture texture)
+{
+    if (Mouse::isButtonPressed(Mouse::Left))
+    {
+        if (changeColor0.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 0;
+                
+            std::cout << currentColor << std::endl;
+        }
+        else if (changeColor1.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 1;
+        }
+        else if (changeColor2.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 2;
+        }
+        else if (changeColor3.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 3;
+        }
+        else if (changeColor4.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 4;
+        }
+        else if (changeColor5.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 5;
+        }
+        else if (changeColor6.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 6;
+        }
+        else if (changeColor7.check(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        {
+            currentColor = 7;
+        }
+        else
+        {
+            texture.update(dessine(Mouse::getPosition(window).x,Mouse::getPosition(window).y,Drawsize,colorTab[currentColor],texture));
+        }
+    }
+    return texture;
+}
 void main()
 {
+    window.setFramerateLimit(60);
     
     CircleShape shape(50); // Rayon de 50 pixels
     shape.setFillColor(Color::Green);
@@ -53,26 +109,30 @@ void main()
         if (Keyboard::isKeyPressed(Keyboard::Left))
         {
             Drawsize--;
-            std::cout << Drawsize << std::endl;
         }
         if (Keyboard::isKeyPressed(Keyboard::Right))
         {
             Drawsize++;
         }
-        if (Mouse::isButtonPressed(Mouse::Left))
-        {
-            texture.update(dessine(Mouse::getPosition(window).x,Mouse::getPosition(window).y,Drawsize,texture));
-        }
         if (Mouse::isButtonPressed(Mouse::Right))
         {
             texture.update(createimage(window.getSize().x,window.getSize().y));
         }
+        texture = changeColor(texture);
         texture.setSmooth(true);
         // Effacer la fenêtre
         window.clear();
         // Dessiner la forme
         Sprite sprite(texture);
         window.draw(sprite);
+        window.draw(changeColor0.display(Color::Black));
+        window.draw(changeColor1.display(Color::Blue));
+        window.draw(changeColor2.display(Color::Cyan));
+        window.draw(changeColor3.display(Color::Green));
+        window.draw(changeColor4.display(Color::Magenta));
+        window.draw(changeColor5.display(Color::Red));
+        window.draw(changeColor6.display(Color::White));
+        window.draw(changeColor7.display(Color::Yellow));
         // Afficher le contenu
         window.display();
     }
